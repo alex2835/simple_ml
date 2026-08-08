9ff7c1fbda749bc45840fd1c3cc8c1cf8880d9bb
Alexander Yushenko <alexander.iushchenko@glidewelldental.com>
2026-08-08 09:32:41 +0400

    init

---
+++ src/layer.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <Eigen/Dense>
+using Mat = Eigen::MatrixXf;
+using Vec = Eigen::VectorXf;
+
+struct Dense
+{
+    Mat W, dW; // [m x n]
+    Vec b, db; // [m]
+    Mat X;     // cached input, [n x B]
+
+    Dense(int n, int m)
+        : W(Mat::Random(m, n) * std::sqrt(2.0f / n)),
+          dW(Mat::Zero(m, n)),
+          b(Vec::Zero(m)),
+          db(Vec::Zero(m))
+    {}
+
+    Mat forward(const Mat &x)
+    {
+        X = x;
+        return (W * X).colwise() + b; // [m x B]
+    }
+
+    Mat backward(const Mat &dY)
+    {
+        dW = dY * X.transpose();   // [m x B][B x n] -> [m x n]
+        db = dY.rowwise().sum();   // [m]
+        return W.transpose() * dY; // [n x m][m x B] -> [n x B]
+    }
+
+    void step(float lr)
+    {
+        W -= lr * dW;
+        b -= lr * db;
+    }
+};
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from " << "gidit_classification" << "!\n";
+    return 0;
+}
